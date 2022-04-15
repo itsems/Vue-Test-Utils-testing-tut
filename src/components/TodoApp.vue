7fa22a068664c5d0d4d9fce50f92905db444f8fc
emmalin <emma.lin@auo.com>
2022-04-15 14:17:54 +0800

    Triggering Events

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -3,6 +3,10 @@
     <div v-for="todo in todos" :key="todo.id" data-test="todo">
       {{ todo.text }}
     </div>
+
+    <form data-test="form" @submit.prevent="createTodo">
+      <input data-test="new-todo" v-model="newTodo" />
+    </form>
   </div>
 </template>
 
@@ -11,6 +15,7 @@ export default {
   name: 'TodoApp',
   data(){
     return {
+      newTodo: '',
       todos: [
         {
           id: 1,
@@ -18,6 +23,16 @@ export default {
           complted: false
         }
       ]
+    }
+  },
+  methods: {
+    createTodo() {
+      this.todos.push({
+        id: new Date().getTime(),
+        text: this.newTodo,
+        complted: false
+      })
+      this.newTodo = ''
     }
   }
 }
--- tests/unit/TodoApp.spec.js
+++ tests/unit/TodoApp.spec.js
@@ -10,4 +10,20 @@ describe('TodoApp', () =>{
 
     expect(todo.text()).toBe('qwer')
   })
+  it('add new todo', async () => {
+    const wrapper = mount(TodoApp);
+    const todoList = '[data-test="todo"]';
+    const newTodoInput = '[data-test="new-todo"]';
+    const todoForm = '[data-test="form"]'
+    
+    expect(wrapper.findAll(todoList)).toHaveLength(1)
+    
+    //! 在 vue 改變 data 的行為是非同步行為(changing state is async in vue)
+    await wrapper.get(newTodoInput).setValue('asdf') // 非同步行為
+    await wrapper.get(todoForm).trigger('submit') // 非同步行為
+    
+    expect(wrapper.findAll(todoList)).toHaveLength(2);
+    expect(wrapper.get(newTodoInput).element.value).toEqual("");
+  })
+
 })
